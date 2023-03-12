3e566ffb2ddabacac403dc25798055345aedb56f
nathandinh <dinhtrongnhan2015@gmail.com>
2023-03-11 22:01:32 -0500

    log write operators

--- tools/log.cpp
+++ tools/log.cpp
@@ -1,6 +1,60 @@
 #include "log.h"
 
-Log::Log()
-{
 
+Log::Log() {
+    fh.open("./exampleLogFile.txt", std::ofstream::out | std::ofstream::app ); //file handler is opened
+    //SHOW_WHERE;
+}
+
+Log::Log(std::string fileName): filePath(fileName), state("open") {
+    fh.open(fileName, std::ofstream::out | std::ofstream::app); //file handler is opened with given file name
+    //SHOW_WHERE;
+}
+
+Log::Log(Log &obj) {
+    fileName = obj.fileName;
+    state = obj.state;
+    filePath = obj.filePath;
+    //SHOW_WHERE;
+}
+
+void Log::operator=(Log &obj) {
+    fileName = obj.fileName;
+    state = obj.state;
+    filePath = obj.filePath;
+    //SHOW_WHERE;
+}
+
+Log::~Log() {
+    fh.close(); //file handler is closed
+    state = "closed"; //set state as closed
+}
+
+Log& Log::operator<<(const std::string num) {
+    try {
+        fh << num; //processes the incoming string
+    } catch(runtime_error) {
+        "File is not open";
+    }
+    return *this; //returns itself as a reference
+}
+
+Log& Log::operator<<(const int num) {
+    try {
+        fh << num; //processes the incoming integer
+    }
+    catch (runtime_error) {
+        "File is not open";
+    }
+    return *this; //returns itself as a reference
+}
+
+Log& Log::operator<<(const double num) {
+    try {
+        fh << num; //processes the incoming double
+    }
+    catch(runtime_error) {
+        "File is not open";
+    }
+    return *this; //returns itself as a reference
 }
--- tools/log.h
+++ tools/log.h
@@ -1,11 +1,120 @@
-#ifndef LOG_H
-#define LOG_H
+#ifndef DOTH_H
+#define DOTH_H
 
-
-class Log
+#include <fstream>
+#include <string>
+#include <filesystem>
+#include "baseclass.h"
+#include "macro.h"
+using namespace std;
+class Log : public BaseClass
 {
+
 public:
+
+    /**
+    * @brief Log default constructor that opens file
+    */
     Log();
+
+    /**
+     * @brief Log parameritized constructor
+     * @param fileName the name of the file
+     */
+    Log(std::string fileName);
+
+    /**
+     * @brief Log_copy copy constructor
+     * @param obj log object
+     */
+    Log(Log &obj);
+
+    /**
+     * @brief operator = copy's assignment
+     * @param obj log object
+     */
+    void operator=(Log &obj);
+
+    ~Log();// destructor
+
+    /**
+     * @brief operator <<
+     * @param str incoming string
+     * @return the overloaded operator as a reference
+     */
+    Log& operator<<(const std::string str);
+
+    /**
+     * @brief operator <<
+     * @param num incoming integer
+     * @return the overloaded operator as a reference
+     */
+
+    Log& operator<<(const int num);
+
+    /**
+     * @brief operator <<
+     * @param num incoming double
+     * @return the overloaded operator as a reference
+     */
+    Log& operator<<(const double num);
+
+    /**
+     * @brief openLogEmpty opens file as an empty file
+     */
+    void open_log_empty();
+
+    /**
+     * @brief openLogAppend appends additional lines to the end of an existing file
+     */
+    void open_log_append();
+
+    /**
+     * @brief closeLog
+     */
+    void close_log();
+
+    /**
+     * @brief flushLog
+     */
+    void flush_log();
+
+    /**
+     * @brief getState getter method for state
+     * @return state
+     */
+    std::string get_state();
+
+    /**
+     * @brief getFileName getter method for file name
+     * @return the file name
+     */
+    std::string get_fileName();
+
+    /**
+     * @brief getFilePath getter method for file path
+     * @return the file path
+     */
+    std::string get_filePath();
+
+private:
+    /**
+     * @brief fileName the file name
+     */
+    string fileName;
+    /**
+     * @brief state the state
+     */
+    string state;
+    /**
+     * @brief filePath the file path
+     */
+    string filePath;
+    /**
+     * @brief fh the file handler
+     */
+    std::ofstream fh;
+
 };
 
-#endif // LOG_H
+#endif // DOTH_H
