0939b85bd74dc646d2aba976d09c5b89b1fc3222
nathandinh <dinhtrongnhan2015@gmail.com>
2023-03-20 22:26:50 -0400

    config tests

--- tools-tests/main.cpp
+++ tools-tests/main.cpp
@@ -1,6 +1,82 @@
 #include <iostream>
 #include "gtest/gtest.h"
 #include "../tools/log.h"
+#include "../tools/config.h"
+
+// The fixture for testing class Foo.
+class FooTest : public ::testing::Test {
+protected:
+    // You can remove any or all of the following functions if its body
+    // is empty.
+    FooTest() {
+        // You can do set-up work for each test here.
+    }
+    virtual ~FooTest() {
+        // You can do clean-up work that doesn’t throw exceptions here.
+    }
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+    virtual void SetUp() {
+        // Code here will be called immediately after the constructor (right
+        // before each test).
+    }
+    virtual void TearDown() {
+        // Code here will be called immediately after each test (right
+        // before the destructor).
+    }
+    // Objects declared here can be used by all tests in the test case for Foo.
+    /** replace with your own **/
+    Config s1;
+    Config s2;
+};
+
+TEST(general, TESTSIMPLE) {
+    /** replace with your own **/
+    Config s1;
+    Config s2("exampleConfigFile.txt");
+    ASSERT_EQ(s1.get_fileName(), s2.get_fileName()) << s1.get_fileName()
+                                                    << " and "
+                                                    << s2.get_fileName()
+                                                    << " should match!";
+}
+
+TEST(general, TESTSTORE){
+    Config c1("test.txt");
+    c1.add_config("type", "orange");
+    //c1.add_config("wood", "knocked");
+    c1.store_configs();
+    fstream fh;
+    string configstr = "";
+    fh.open("test.txt", ios::in); //opens file
+    while(fh){ //reads file
+        string line;
+        getline(fh, line);
+        string tempConfig = line.substr(0, line.find(":"));
+        string tempValue = line.substr(line.find(":")+1, line.length());
+
+        string temp = tempConfig + ":" + tempValue;
+        configstr += temp;
+    }
+    ASSERT_EQ(configstr, "type:orange:") << "Configs should match";
+}
+
+TEST(general, TESTLOAD){
+    Config c1("test2.txt");
+
+    fstream fh;
+    fh.open("test2.txt");
+    fh << "wood:knocked" << "\n";
+    fh.close();
+    c1.load_configs();
+
+    map<string, string> configmap = c1.get_configs();
+    string configstr = "";
+    for (auto item = configmap.begin(); item != configmap.end(); item++){ //loops through configs
+        string temp = "";
+        configstr = item->first + ":" + item->second +"\n"; //temp is the key then the value
+    }
+    ASSERT_EQ(configstr, "wood:knocked\n") << "Loaded configs should match";
+}
 // The fixture for testing class Log.
 class LogTest : public ::testing::Test {
 protected:
@@ -27,7 +103,7 @@ protected:
     Log s2;
 };
 
-TEST(general, TESTSIMPLE) {
+TEST(general, TESTSIMPLE1) {
     Log s1; //first log object
     Log s2("exampleLogFile.txt"); //second log object
     ASSERT_EQ(s1.get_fileName(), s2.get_fileName()) << s1.get_fileName()
